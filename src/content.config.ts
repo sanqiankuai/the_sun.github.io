311bbf163c9c676fbec6175bfed9a9e015be75f2
the_sun <your_email@example.com>
2026-04-26 11:41:13 +0800

    Update homepage card for post 260426

--- src/content.config.ts
+++ src/content.config.ts
@@ -10,6 +10,7 @@ const blog = defineCollection({
 			description: z.string(),
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
+			cardImage: z.string().optional(),
 			heroImage: z.optional(image()),
 		}),
 });
